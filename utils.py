4eceafd9cc34994e10b4a122d33a2ac8b5bb4ae1
Michael Krumdick <mrkrumdick@gmail.com>
2022-01-26 00:48:29 +0000

    Initial rough commit of code pulled out of stonefish

---
+++ utils.py
@@ -0,0 +1,141 @@
+
+#from stonefish.train.generate import get_lm_input
+#from t5 import get_outputs, write_output
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from datasets import load_dataset
+
+class CommonGenEval(Dataset):
+
+    tokenizer = None
+
+    def __init__(self, split, tokenizer):
+        dataset = load_dataset("common_gen")
+        CommonGenEval.tokenizer = tokenizer #< Gross!
+
+        dataset = dataset[split]
+
+        self.dataset = {}
+
+        for d in dataset:
+            id_ = d["concept_set_idx"]
+
+            if id_ not in self.dataset.keys():
+                self.dataset[id_] = (" ".join(d["concepts"]), [])
+
+            self.dataset[id_][1].append(d["target"])
+
+    def __getitem__(self, idx):
+        data = self.dataset[idx]
+        return data[0], [data[1]]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    @classmethod
+    def collate_fn(cls, batch):
+        starters, targets = zip(*batch)
+        return cls.tokenizer(list(starters), padding=True, return_tensors="pt"), targets
+
+def get_lm_input(
+    self,
+    input_ids=None,
+    attention_mask=None,
+    decoder_input_ids=None,
+    decoder_attention_mask=None,
+    head_mask=None,
+    decoder_head_mask=None,
+    cross_attn_head_mask=None,
+    encoder_outputs=None,
+    past_key_values=None,
+    inputs_embeds=None,
+    decoder_inputs_embeds=None,
+    labels=None,
+    use_cache=None,
+    output_attentions=None,
+    output_hidden_states=None,
+    return_dict=None,
+):
+    r"""
+    """
+    use_cache = use_cache if use_cache is not None else self.config.use_cache
+    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+    # FutureWarning: head_mask was separated into two input args - head_mask, decoder_head_mask
+    if head_mask is not None and decoder_head_mask is None:
+        if self.config.num_layers == self.config.num_decoder_layers:
+            warnings.warn(__HEAD_MASK_WARNING_MSG, FutureWarning)
+            decoder_head_mask = head_mask
+
+    # Encode if needed (training, first prediction pass)
+    if encoder_outputs is None:
+        # Convert encoder inputs in embeddings if needed
+        encoder_outputs = self.encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+    elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
+        encoder_outputs = BaseModelOutput(
+            last_hidden_state=encoder_outputs[0],
+            hidden_states=encoder_outputs[1] if len(encoder_outputs) > 1 else None,
+            attentions=encoder_outputs[2] if len(encoder_outputs) > 2 else None,
+        )
+
+    hidden_states = encoder_outputs[0]
+
+    if self.model_parallel:
+        torch.cuda.set_device(self.decoder.first_device)
+
+    if labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
+        # get decoder inputs from shifting lm labels to the right
+        decoder_input_ids = self._shift_right(labels)
+
+    # Set device for model parallelism
+    if self.model_parallel:
+        torch.cuda.set_device(self.decoder.first_device)
+        hidden_states = hidden_states.to(self.decoder.first_device)
+        if decoder_input_ids is not None:
+            decoder_input_ids = decoder_input_ids.to(self.decoder.first_device)
+        if attention_mask is not None:
+            attention_mask = attention_mask.to(self.decoder.first_device)
+        if decoder_attention_mask is not None:
+            decoder_attention_mask = decoder_attention_mask.to(self.decoder.first_device)
+
+    # Decode
+    decoder_outputs = self.decoder(
+        input_ids=decoder_input_ids,
+        attention_mask=decoder_attention_mask,
+        inputs_embeds=decoder_inputs_embeds,
+        past_key_values=past_key_values,
+        encoder_hidden_states=hidden_states,
+        encoder_attention_mask=attention_mask,
+        head_mask=decoder_head_mask,
+        cross_attn_head_mask=cross_attn_head_mask,
+        use_cache=use_cache,
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+        return_dict=return_dict,
+    )
+
+    sequence_output = decoder_outputs[0]
+
+    # Set device for model parallelism
+    if self.model_parallel:
+        torch.cuda.set_device(self.encoder.first_device)
+        self.lm_head = self.lm_head.to(self.encoder.first_device)
+        sequence_output = sequence_output.to(self.lm_head.weight.device)
+
+    if self.config.tie_word_embeddings:
+        # Rescale output before projecting on vocab
+        # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
+        sequence_output = sequence_output * (self.model_dim ** -0.5)
+
+    return sequence_output
+
